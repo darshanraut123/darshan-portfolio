aa04237191d0a5bf032124ae174b27928330107b
darshanraut123 <darshanraut3828@gmail.com>
2022-03-28 14:39:29 +0530

    Added few corrections

--- src/components/data/projects_data.js
+++ src/components/data/projects_data.js
@@ -2,6 +2,7 @@ import urlshortener from '../../assets/projects_images/urlshortener.png'
 import reset from '../../assets/projects_images/reset.png';
 import webscrapping from '../../assets/projects_images/webscrapping.png';
 import foodstore from '../../assets/projects_images/foodstore.png';
+import portfolio from '../../assets/projects_images/portfolio.png';
 
 
 const data_projects = [
@@ -12,19 +13,25 @@ const data_projects = [
         github_url: 'https://github.com/darshanraut123/urlshortner-frontend',
     },
     {
-        name: 'Password Reset Flow',
-        image: reset,
-        deployed_url: 'https://password-reset-flow-frontend.netlify.app/',
-        github_url: 'https://github.com/darshanraut123/password-reset-flow-backend',
-    },
-    {
         name: 'WebScrapping',
         image: webscrapping,
         deployed_url: 'https://webscrappingecommercefrontend.herokuapp.com/',
         github_url: '//https://github.com/darshanraut123/webScrapping-Ecommerce',
     },
     {
-        name: 'Food Store CRUD',
+        name: 'Password Reset Email verification',
+        image: reset,
+        deployed_url: 'https://password-reset-flow-frontend.netlify.app/',
+        github_url: 'https://github.com/darshanraut123/password-reset-flow-backend',
+    },
+    {
+        name: 'Current Portfolio',
+        image: portfolio,
+        deployed_url: 'https://darshans-portfolio.netlify.app/',
+        github_url: 'https://github.com/darshanraut123/darshan-portfolio',
+    },
+    {
+        name: 'Food Store CRUD Front END',
         image: foodstore,
         deployed_url: 'https://food-choice-app.netlify.app/',
         github_url: 'https://github.com/darshanraut123/CRUD-Formik-Food-Choice-App-27-28',
